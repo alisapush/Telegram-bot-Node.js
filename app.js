3f3441e7732077c41bc2db5b46efa474ba4a5673
Alisa Pushnova <61162236+alisapush@users.noreply.github.com>
2021-12-03 13:23:29 +0300

    add waiting for action

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const TelegramApi = require("node-telegram-bot-api");
-const token = "2057843551:AAGybFyGuz0xrx7p2Eawq3EaU5Kzw56wNjM";
+const token = "5025111280:AAGekOPhDA29afw27pxjOevSqie7cTYQDGA";
 
 const stickerStart =
   "https://tlgrm.ru/_/stickers/51e/d68/51ed68e0-9cee-39cc-bebe-24ea15a45442/5.webp";
@@ -24,79 +24,80 @@ const categories = {
   }),
 };
 
-const transactions = [
-  // { amount: , category_id: , user_id: },
-];
-
-const users = [
-  // { user_id: "", last_amount: ""},
-];
+const chats = new Map();
 
 const start = () => {
   bot.setMyCommands([
     { command: "/start", description: "Начальное приветствие" },
-    { command: "/info", description: "Получить информацию о пользователе" },
+    { command: "/info", description: "Получить информацию о транзакциях" },
+    { command: "/add", description: "Добавить" },
   ]);
 
   bot.on("message", async (msg) => {
     const chatId = msg.chat.id;
+    const userId = msg.from.id;
     const text = msg.text;
 
-    if (text === "/start") {
-      let user = {
-        user_id: chatId,
-        last_amount: 0,
-      };
-      users.push(user);
+    console.log("chatId", chatId, "userId", userId);
 
-      await bot.sendSticker(chatId, stickerStart);
-      return bot.sendMessage(
-        chatId,
-        "Добро пожаловать в Statistics! Введите сумму:"
-      );
+    let chat;
+    if (chats.has(chatId)) {
+      chat = chats.get(chatId);
+    } else {
+      chat = { state: "WAITING_FOR_ACTION" };
+      chats.set(chatId, chat);
     }
 
-    if (isNumeric(text)) {
-      let current_user = users.find((user) => user.user_id === chatId);
-      current_user.last_amount = Number(text);
-      return bot.sendMessage(chatId, "Выберите категорию", categories);
+    switch (chat.state) {
+      case "WAITING_FOR_ACTION": {
+        if (text === "/start") {
+          bot.sendMessage(chatId, "Добро пожаловать");
+        } else if (text === "/add") {
+          chat.state = "WAITING_FOR_CATEGORY_SELECT";
+          bot.sendMessage(chatId, "Выберите категорию", categories);
+        } else {
+          bot.sendMessage(chatId, "Неверная команда");
+        }
+
+        break;
+      }
+      case "WAITING_FOR_SUM_ENTER": {
+        if (isNumeric(text)) {
+          chat.enteredSum = text;
+          bot.sendMessage(
+            chatId,
+            `Платеж добавлен: ${chat.selectedCategory} ${chat.enteredSum}`
+          );
+          chat.state = "WAITING_FOR_ACTION";
+        } else {
+          bot.sendMessage(chatId, "Сумма введена некорректно");
+        }
+        break;
+      }
+      default: {
+        bot.sendMessage(chatId, "Что-то пошло не так");
+        chat.state = "WAITING_FOR_ACTION";
+      }
     }
-
-    if (text === "/info") {
-      await bot.sendMessage(
-        chatId,
-        `Вас зовут ${msg.from.first_name} ${msg.from.last_name}`
-      );
-
-      let ts = transactions.filter(
-        (transaction) => transaction.user_id === chatId
-      );
-      let mes = JSON.stringify(ts);
-
-      return bot.sendMessage(chatId, `Транзакции: ${mes}`);
-    }
-
-    return bot.sendMessage(chatId, "Я Вас не понимаю, попробуйте еще раз!");
   });
 
   bot.on("callback_query", async (msg) => {
     const category = msg.data;
-    console.log(msg);
     const chatId = msg.message.chat.id;
+    const chat = chats.get(chatId);
 
-    let current_user = users.find((user) => user.user_id === chatId);
+    switch (chat.state) {
+      case "WAITING_FOR_CATEGORY_SELECT": {
+        chat.selectedCategory = category;
+        chat.state = "WAITING_FOR_SUM_ENTER";
+        bot.sendMessage(chatId, "Введите сумму");
 
-    let transaction = {
-      user_id: chatId,
-      category_id: category,
-      amount: current_user.last_amount,
-    };
-
-    transactions.push(transaction);
-
-    await bot.sendMessage(chatId, `Добавлено в категорию ${category}`);
-    await bot.sendSticker(chatId, stickerAdd);
-    console.log(msg);
+        break;
+      }
+      default: {
+        console.log("Something went wrong");
+      }
+    }
   });
 };
 
